a80df70eec3526c4bd35721747b1a3285525c999
lukasp1998 <lukas@DESKTOP-T80SMT9.localdomain>
2020-12-03 16:08:49 +0100

    test System vollständig

--- main.cpp
+++ main.cpp
@@ -1,5 +1,4 @@
 #include <iostream>
-#include <filesystem>
 #include "helpSystem.hpp"
 
 int main() {
@@ -22,6 +21,19 @@ int main() {
         for (Entry *e : entries) {
             std::cout << e->name << std::endl;
         }
+
+        if(!entries.empty()) {
+            int number;
+            std::cout << "Show Text (0) ?" << std::endl;
+            std::cin >> number;
+            if(number == 0){
+
+            } else {
+                std::cout << entries.at(number)->name << std::endl;
+                std::cout << entries.at(number)->content << std::endl;
+            }
+        }
+
         std::cout << std::endl;
     }
 
